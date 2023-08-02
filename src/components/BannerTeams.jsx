4668044a6fd798f1ecbdfabef73835067e979094
David Baquero <davidbaquerogonzalez@gmail.com>
2023-08-02 19:14:56 +0200

    id and class gototeams GA4
    id and class gototeams GA4

--- src/components/BannerTeams.jsx
+++ src/components/BannerTeams.jsx
@@ -13,9 +13,9 @@ function BannerTeams() {
                         ¿No conoces a los equipos, conferencias y divisiones? Haz clic en el botón de abajo y... ¡touchdown! Te llevaremos directo a la zona de los equipos. ¡Que empiece el espectáculo!
                     </p>
                     <div className="d-flex justify-content-center">
-                        <a href="#teams" className="d-flex align-items-center link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover btn-iraequipos">
+                        <a href="#teams" className="d-flex align-items-center link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover btn-gototeams">
                             <h4 className="mx-3 btnanchorteams">Ir a equipos y conferencias</h4>
-                            <ArrowDownwardIcon fontSize="large" sx={{ color: "#FFD558" }} />
+                            <ArrowDownwardIcon id="btn-arrowgototeams" fontSize="large" sx={{ color: "#FFD558" }} />
                         </a>
                     </div>
                 </div>
