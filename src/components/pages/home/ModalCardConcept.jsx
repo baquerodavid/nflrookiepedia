6848a8e26c18e4e4936c8c78d7b0a2280ab36ff7
David Baquero <davidbaquerogonzalez@gmail.com>
2024-05-09 14:01:26 +0200

    Update NavBar and others
    Update

--- src/components/pages/home/ModalCardConcept.jsx
+++ src/components/pages/home/ModalCardConcept.jsx
@@ -10,104 +10,103 @@ import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import { marked } from 'marked';
 
-
 const ButtonReadMore = styled(Button)({
-  boxShadow: 'none',
-  textTransform: 'none',
-  padding: '4px 8px',
-  border: '1px solid #D0D5DD',
-  borderRadius: "8px",
-  lineHeight: 1.5,
-  color: "#344054",
-  fontFamily: [
-    'Roboto',
-  ].join(','),
-  '&:hover': {
-    backgroundColor: "#F8F8F8",
-    borderColor: '#D0D5DD',
-    boxShadow: '0px 1px 2px 0px rgb(16, 24, 40, 0.05)',
-  },
+    boxShadow: 'none',
+    textTransform: 'none',
+    padding: '4px 8px',
+    border: '1px solid #D0D5DD',
+    borderRadius: "8px",
+    lineHeight: 1.5,
+    color: "#344054",
+    fontFamily: [
+        'Roboto',
+    ].join(','),
+    '&:hover': {
+        backgroundColor: "#F8F8F8",
+        borderColor: '#D0D5DD',
+        boxShadow: '0px 1px 2px 0px rgb(16, 24, 40, 0.05)',
+    },
 });
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
-  },
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
 }));
 
 function BootstrapDialogTitle(props) {
-  const { children, onClose, ...other } = props;
+    const { children, onClose, ...other } = props;
 
-  return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-      {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </DialogTitle>
-  );
+    return (
+        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+            {children}
+            {onClose ? (
+                <IconButton
+                aria-label="close"
+                onClick={onClose}
+                sx={{
+                    position: 'absolute',
+                    right: 8,
+                    top: 8,
+                    color: (theme) => theme.palette.grey[500],
+                }}
+                >
+                <CloseIcon />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
+    );
 }
 
 BootstrapDialogTitle.propTypes = {
-  children: PropTypes.node,
-  onClose: PropTypes.func.isRequired,
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
 };
 
 function ModalCardConcept({ cardTitle, description }) {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      <ButtonReadMore id="btn-readmore" size="small" onClick={handleClickOpen}
-        sx={{
-          position: "absolute",
-          left: 16,
-          bottom: 12,
-          '&:active': {
-            bgcolor: 'text.secondary',
-            color: 'background.paper',
-            boxShadow: '0px 0px 0px 3px #F2F4F7, 0px 1px 2px 0px rgb(16, 24, 40, 0.05)',
-          },
-        }}>
-        Leer más
-      </ButtonReadMore>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <BootstrapDialogTitle id="customized-dialog-title" variant="h5" onClose={handleClose}>
-            {cardTitle}
-        </BootstrapDialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            <div className="cardtextnfl" dangerouslySetInnerHTML={{ __html: marked(description) }}></div>
-          </Typography>
-        </DialogContent>
-      </BootstrapDialog>
-    </div>
-  );
+    return (
+        <div>
+            <ButtonReadMore id="btn-readmore" size="small" onClick={handleClickOpen}
+                sx={{
+                position: "absolute",
+                left: 16,
+                bottom: 12,
+                '&:active': {
+                    bgcolor: 'text.secondary',
+                    color: 'background.paper',
+                    boxShadow: '0px 0px 0px 3px #F2F4F7, 0px 1px 2px 0px rgb(16, 24, 40, 0.05)',
+                },
+                }}>
+                Leer más
+            </ButtonReadMore>
+            <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <BootstrapDialogTitle id="customized-dialog-title" variant="h5" onClose={handleClose}>
+                    {cardTitle}
+                </BootstrapDialogTitle>
+                <DialogContent dividers>
+                <Typography gutterBottom>
+                    <div className="cardtextnfl" dangerouslySetInnerHTML={{ __html: marked(description) }}></div>
+                </Typography>
+                </DialogContent>
+            </BootstrapDialog>
+        </div>
+    );
 }
 
 export default ModalCardConcept;
--- src/components/pages/home/SkeletonCard.jsx
+++ src/components/pages/home/SkeletonCard.jsx
@@ -3,32 +3,32 @@ import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
 
 function SkeletonCard() {
-  const repeatCount = 9;
+	const repeatCount = 9;
 
-  const renderSkeletonStack = () => {
-    return Array.from({ length: repeatCount }, (_, index) => (
-      <Stack key={index} spacing={1.5} alignItems="left" width={400}>
-        <Skeleton variant="rounded" height={35} />
-        <Skeleton height={40} width="80%" />
-        <Skeleton height={15} />
-        <Skeleton height={15} />
-        <Skeleton height={15} />
-        <Skeleton variant="rounded" height={25} width={60} />
-      </Stack>
-    ));
-  };
+	const renderSkeletonStack = () => {
+		return Array.from({ length: repeatCount }, (_, index) => (
+			<Stack key={index} spacing={1.5} alignItems="left" width={400}>
+				<Skeleton variant="rounded" height={35} />
+				<Skeleton height={40} width="80%" />
+				<Skeleton height={15} />
+				<Skeleton height={15} />
+				<Skeleton height={15} />
+				<Skeleton variant="rounded" height={25} width={60} />
+			</Stack>
+		));
+	};
 
-  return (
-      <Stack
-        width="100%"
-        direction={{ xs: "column", sm: "row" }}
-        spacing={{ xs: 3, sm: 6 }}
-        useFlexGap
-        flexWrap="wrap"
-    >
-      {renderSkeletonStack()}
-      </Stack>
-  );
+	return (
+		<Stack
+			width="100%"
+			direction={{ xs: "column", sm: "row" }}
+			spacing={{ xs: 3, sm: 6 }}
+			useFlexGap
+			flexWrap="wrap"
+		>
+			{renderSkeletonStack()}
+		</Stack>
+	);
 }
 
 export default SkeletonCard;
